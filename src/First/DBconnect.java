31c8e773f2efd5a4f684e932e73e093bc3cea26f
sahanmadu <sahanmadusanka640@gmail.com>
2019-10-28 19:34:59 +0530

    This is a text file

---
+++ src/First/DBconnect.java
@@ -0,0 +1,29 @@
+
+package First;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+
+public class DBconnect {
+    public static Connection connect()
+    {
+        Connection conn=null;
+        
+        try{
+        
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/Testproject","root","");
+        
+        }
+        catch(Exception e)
+            
+        {
+            System.out.println(e);
+        
+        }
+        
+        return conn;
+    }
+    
+}
